3bc748374a060c8299fcd2a448ef6e7839530e3e
malmiameesha <malmiameesha123@gmail.com>
2024-12-14 05:41:20 +0530

    Create QuickFit.Py

---
+++ QuickFit.py
@@ -0,0 +1,5 @@
+class QuickFit:
+    def __init__(self):
+        self.memory = {50:{"block1":50,"block2":50},100:{"block3":100,"block4":100},200:{"block5":200}}
+        #self.processSize = processSize
+
